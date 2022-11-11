b8fba0eb795f17dfdf9567dfa8a5450c7e237a5a
hammodd <101920360+hammodd@users.noreply.github.com>
2022-11-11 22:26:09 +0300

    rind

---
+++ java1.java
@@ -0,0 +1,169 @@
+ /* */
+
+var ham={
+    name:"hamedh",
+    age:20,
+    father:"hassn",
+  
+    sg:function(){
+        console.log("hello");
+    } ,
+     zhr:{
+        name:"zhra",
+        age:20,
+    }
+}
+        console.log(ham.name);
+        console.log(ham.age);
+        console.log(ham.father);
+        console.log(ham.zhr.name);
+        console.log(ham.zhr.age);
+        console.log("hello");
+        ham.sg();
+
+
+
+
+ /*console.log("hamedh")
+تعريف المتغيرات   
+ var  ,let   ,conts */
+ /*
+y=10
+z=12
+x=5
+console.log("z")
+console.log(z+y)
+a=8
+b=3
+
+c=a
+a=b
+b=c
+console.log("b="+b)
+console.log("c="+c)
+console.log("a="+a)
+
+alert("hamedh")
+s=prompt("nnnn")
+n=Number(prompt("n="))
+m=Number(prompt("m="))
+mom=n+m
+console.log(mom)
+
+console.log("ادخل العمر")
+m=Number(prompt(" ادخل العمر"))
+f=2022-m
+console.log(f)
+ */
+ /*
+a=Number(prompt("enter in="))
+if(a>0){
+    console.log("test a")
+}
+else if(a==0){
+    console.log("a is zero")
+}
+else{
+    console.log("not a")
+}
+
+x=Number(prompt("enter in x="))
+y=Number(prompt("enter in y="))
+c=prompt("enter the -,+,*,/")
+if(c=="+"){
+    console.log(x+y)
+}
+else if(c=="-"){
+    console.log(x-y)
+}
+ else if(c=="*"){
+    console.log(x*y)
+}
+ else if(c=="/"){
+    console.log(x/y)
+}
+ else {
+    console.log("enter ERERR")
+}
+ */
+/*
+مصفوفه احاديه تحسب جدول الضرب لواحد فقط مثلا جدول الضرب لل1 
+for(i=0;i<10;i++){
+      console.log(i+"*"+"10="+(i*10))   
+     
+}
+
+for(i=0;i<10;i++){
+  z=i*10
+      console.log(i+"* 10 ="+z)     
+}
+
+  مصفوفه متداخله وتاخذ جدول الضرب من 1 الى10
+for(i=1;i<=10;i++){
+    for(j=1;j<=10;j++){
+        z=i*j
+      console.log(i+"*"+j+"="+z)     
+}  
+}
+
+*/
+/*
+x=[2,3,4,5,7,"ham",98767,"hamedh"]
+console.log(x[6]);
+
+console.log("_______")
+
+x=[2,3,4,5,7,"ham",98767,"hamedh"]
+for(i=0;i<8;i++){
+    
+       console.log(x[i])     
+}
+
+
+function add(){
+    x=1
+    y=6
+    z=x+y
+    console.log(z)
+}
+add()
+
+function sdd(x,y){
+    z=x+y
+    console.log(z)
+}
+sdd(9,8)
+sdd(4,5)
+
+function mul(x,y){
+    
+    z=x*y
+    console.log(z)
+}
+a=Number(prompt("A="))
+b=Number(prompt("B="))
+mul(a,b)
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ rund.js
@@ -0,0 +1,6 @@
+var rundomImag1=Math.floor(Math.random()*13)+1;
+var imagDice1="img/dice"+rundomImag1+".png"
+var img1=document.querySelectorAll("img")[0]
+img1.setAttribute("src",imagDice1)
+ 
+
